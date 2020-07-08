813c8b86cc8aa3464311469607be89b4d5aaf9c0
root <root@gj-isombe.localdomain>
2020-07-08 18:01:50 -0400

    added state mgmt using redux

--- src/reducers.js
+++ src/reducers.js
@@ -1,10 +1,21 @@
-import {CHANGE_SEARCH_FIELD} from './constants'
+import {
+    CHANGE_SEARCH_FIELD,
+    REQUEST_ROBOTS_PENDING,
+    REQUEST_ROBOTS_SUCCESS,
+    REQUEST_ROBOTS_FAILED
+} from './constants'
 
-const initialState = {
+const initialSearchState = {
     searchField: ''
 }
 
-export const searchRobots = (state = initialState, action = {}) => {
+const initialRobotsState = {
+    isPending: false,
+    robots: [],
+    error: ''
+}
+
+export const searchRobots = (state = initialSearchState, action = {}) => {
     switch(action.type){
         case CHANGE_SEARCH_FIELD:
             return Object.assign(
@@ -15,5 +26,18 @@ export const searchRobots = (state = initialState, action = {}) => {
         
         default:
             return state;
+    }
+}
+
+export const requestRobots = (state=initialRobotsState, action={}) => {
+    switch(action.type){
+        case REQUEST_ROBOTS_PENDING:
+            return Object.assign({}, state, {isPending: true});
+        case REQUEST_ROBOTS_SUCCESS:
+            return Object.assign({}, state, {robots: action.payload, isPending: false})
+        case REQUEST_ROBOTS_FAILED:
+            return Object.assign({}, state, {error: action.payload, isPending: false})
+        default:
+            return state
     }
 }
